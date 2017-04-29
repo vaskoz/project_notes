446c1b49ba5b49475d4ff57ab89c7d0fb4e5fc32
Vasko Zdravevski <vzdravevski@gmail.com>
2017-04-29 11:20:33 -0600

    add countries endpoint

--- hello.py
+++ hello.py
@@ -1,14 +1,21 @@
 from sqlalchemy import create_engine
 from flask import Flask
 import json
+import decimal
 
 app = Flask(__name__)
-engine = create_engine('postgresql://localhost:5432/cities', echo=True)
+engine = create_engine('postgresql://localhost:5432/world', echo=True)
 connection = engine.connect()
 
-@app.route('/')
-def hello_world():
-    result = connection.execute("select name from city")
+@app.route('/cities')
+def list_cities():
+    result = connection.execute("select * from city")
+    s = json.dumps([dict(r) for r in result])
+    return s
+
+@app.route('/countries')
+def list_countries():
+    result = connection.execute("select name from country")
     s = json.dumps([dict(r) for r in result])
     return s
 
