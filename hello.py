a9108845638b3171695615cc9b4b406b3f791d63
Vasko Zdravevski <vzdravevski@gmail.com>
2017-04-29 10:47:17 -0600

    serialize result to json

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
 from flask import Flask
+import json
+
 app = Flask(__name__)
 engine = create_engine('postgresql://localhost:5432/cities', echo=True)
 connection = engine.connect()
@@ -7,10 +9,6 @@ connection = engine.connect()
 @app.route('/')
 def hello_world():
     result = connection.execute("select name from city")
-    s = ""
-    for row in result:
-        s += row['name']
-        s += "\n"
-    connection.close()
+    s = json.dumps([dict(r) for r in result])
     return s
 
